2e86f5fb1d16a3e33c69f270df159fe3f47f37b5
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-07-20 23:49:03 +0530

    Add project files.

---
+++ Collection Dictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Collection_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> d = new Dictionary<int, string>();
+            d.Add(1, "akshay");
+            d.Add(2, "s");
+            d.Add(3, "akshay");
+            d.Add(4, "z");
+            
+
+            foreach (KeyValuePair<int,string> item in d)
+            {
+                Console.WriteLine(" "+item.Key+" "+item.Value);
+            }
+
+          /*  foreach (KeyValuePair<int, string> item in d)
+            {
+                Console.WriteLine(" " + item);
+            }*/
+            Console.WriteLine("Total "+d.Count);
+            
+        }
+    }
+}
